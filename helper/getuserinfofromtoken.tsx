1aeabf756154be5dc9b5987f9bbf8a53fbbc268e
Arihant Jain <arihantj916@gmail.com>
2026-02-10 15:07:49 +0530

    feat: update secretKey import from .env

--- helper/getuserinfofromtoken.tsx
+++ helper/getuserinfofromtoken.tsx
@@ -11,9 +11,7 @@ export default function getUserInfo() {
       };
     }
 
-    const secretKey =
-      process.env.JWT_SECRET! ||
-      "MIIBOgIBAAJBAIkOAiH9QSDo4Zu7sd7NvDFVAyT0NUhdQepKA5r3/zZ0vHdGvEXhqx5ZJ5v6karghyBVQC25UncmnR45d9tI4A8CAwEAAQJAf8abnmyqR7BhgabcgmFJskHQXCcLHclePTAH76OJYmauUTL0jI3N6JjvMZpoPEJA0hUwH6j0a2f5YFwH+VIQIhAMA9euBYLcHxmkjUbw+V5PlqMdsEgpxzjs3SVrvJz2dAioLmuGKAhj93sJXYmBjE93gZaNGLh26R/In0iFOpsIHjR6zyQKp29+Dg553Pc5qPgMgxr4obH8eqLm8HYpyNAiEAnH4NWtiLT/9r7Q4Ab8Wx4R9adzEh9qpkuvXWEyUgDUCIEjXxQ+pRBBT6oOG32PaK6kAamCAa479PmW0egEvPjSR";
+    const secretKey = process.env.NEXT_PUBLIC_JWT_SECRET!;
 
     const decoded = jwt.verify(authCookie.toString(), secretKey);
     if (!decoded) {
